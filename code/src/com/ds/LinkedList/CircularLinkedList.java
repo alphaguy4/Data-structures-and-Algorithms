298959fb2d7e744bb949ebdb5181a462aa23cf5b
Kartikay Singh <kartikays@Kartikays-MacBook-Pro.local>
2021-12-04 15:39:04 +0530

    Implementing Circular LinkedList: Insert, delete, display implemented

---
+++ code/src/com/ds/LinkedList/CircularLinkedList.java
@@ -0,0 +1,71 @@
+package com.ds.LinkedList;
+
+public class CircularLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public CircularLinkedList() {
+        this.size = 0;
+    }
+
+    public void insert(int value) {
+        Node node = new Node(value);
+        if(head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.next = head;
+            tail = node;
+        }
+        size += 1;
+    }
+
+    public void display() {
+        Node node = head;
+
+        do {
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        } while (node != head);
+
+        System.out.println("HEAD");
+        System.out.println("Size = " + size);
+    }
+
+    public void delete(int value) {
+        if(size == 0) {
+            System.err.println("Error !! No elements to delete");
+            return;
+        }
+
+        Node node = head;
+
+        if(node.value == value) {
+            head = head.next;
+            tail.next = head;
+        }
+
+        do {
+            Node nextNode = node.next;
+            if(nextNode.value == value) {
+                node.next = nextNode.next;
+                break;
+            }
+            node = node.next;
+        } while (node != head);
+        size -= 1;
+    }
+
+    static class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
--- code/src/com/ds/LinkedList/Main.java
+++ code/src/com/ds/LinkedList/Main.java
@@ -5,27 +5,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to LinkedList world!");
 
-        DoublyLinkedList dll = new DoublyLinkedList();
-        dll.insertLast(2);
-        dll.insertFirst(12);
-        dll.insertFirst(15);
-        dll.insertFirst(14);
-        dll.display();
-
-        dll.insertAt(102, 2);
-        dll.display();
-
-        dll.insertAt(103, 3);
-        dll.display();
-
-        dll.deleteFirst();
-        dll.display();
-
-        dll.deleteLast();
-        dll.display();
-
-        dll.delete(103);
-        dll.display();
+        CircularLinkedList cll = new CircularLinkedList();
+        cll.insert(12);
+        cll.insert(13);
+        cll.insert(14);
+        cll.insert(15);
+        cll.delete(14);
+        cll.display();
 
     }
 }
