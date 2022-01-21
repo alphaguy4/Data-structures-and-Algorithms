a1a02aedffc9a26a957772a880fd089008b88c48
Kartikay Singh <kartikays@Kartikays-MacBook-Pro.local>
2022-01-21 14:36:07 +0530

    Radix sort

---
+++ code/src/com/algos/sorting/RadixSort.java
@@ -0,0 +1,63 @@
+package com.algos.sorting;
+
+import java.util.stream.IntStream;
+
+/*
+* Only applicable for array with positive integer
+* Uses counting Sort
+* */
+public class RadixSort {
+
+    public static int[] sort(int [] array) {
+        if(array.length == 0) {
+            return array;
+        }
+
+        int maxNumber = IntStream.of(array).max().getAsInt();
+        int digit = 0;
+        while( maxNumber / Math.pow(10, digit) > 0) {
+            countSort(array, digit);
+            digit++;
+        }
+
+        return array;
+    }
+
+    private static void countSort(int[] array, int digit) {
+        int[] sortedArray = new int[array.length];
+        int[] countArray = new int[10];
+
+        // populate the countArray with digit
+        for(int num: array) {
+            int countIdx = (int)(num / Math.pow(10, digit)) % 10;
+            countArray[countIdx] += 1;
+        }
+
+        // preSum
+        for(int idx = 1; idx < countArray.length; idx++) {
+            countArray[idx] += countArray[idx - 1];
+        }
+
+        for(int idx = array.length - 1; idx >= 0; idx--) {
+            int countIdx = (int)(array[idx] / Math.pow(10, digit)) % 10;
+            countArray[countIdx] -= 1;
+
+            int sortedIdx = countArray[countIdx];
+            sortedArray[sortedIdx] = array[idx];
+        }
+
+        for(int i = 0; i < sortedArray.length; i++) {
+            array[i] = sortedArray[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {133, 32, 64, 1992};
+        RadixSort.sort(arr);
+        for(int num: arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+}
